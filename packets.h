e6e899fa4142c74fe12de3afc9c6ddd378e6e3f0
Matthias Wenzel <acab@mazzoo.de>
2010-07-01 00:06:21 +0200

    16 bit color depth definition

--- packets.h
+++ packets.h
@@ -27,10 +27,11 @@
 #define PKT_MASK_VERSION 0xff000000 /* 0x00 */
 #define PKT_MASK_DBL_BUF 0x00008000 /* uses feature double buffering    */
 #define PKT_MASK_FADING  0x00004000 /* uses feature fading              */
-#define PKT_MASK_RGB8    0x00002000 /* r8g8b8 format                    */
-#define PKT_MASK_GREY4   0x00001000 /* 4bit grey per pixel              */
-#define PKT_MASK_BW      0x00000800 /* bw 1 byte per pixel format       */
-#define PKT_MASK_BW_PACK 0x00000400 /* bw pixel 8 pixel per byte format */
+#define PKT_MASK_RGB8    0x00002000 /* r16g16b16 format                 */
+#define PKT_MASK_RGB8    0x00001000 /* r8g8b8 format                    */
+#define PKT_MASK_GREY4   0x00000800 /* 4bit grey per pixel              */
+#define PKT_MASK_BW      0x00000400 /* bw 1 byte per pixel format       */
+#define PKT_MASK_BW_PACK 0x00000200 /* bw pixel 8 pixel per byte format */
 #define PKT_MASK_TYPE    0x000000ff
 
 #define PKT_TYPE_SET_SCREEN_BLK     0x00
